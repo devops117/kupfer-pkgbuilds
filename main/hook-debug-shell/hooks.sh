05f8a4c7f35ee00ff45cd4ab59c20b6ab3f10f99
jld3103 <jld3103yt@gmail.com>
2021-08-17 16:09:43 +0200

    Add debug-shell initramfs hook

---
+++ main/hook-debug-shell/hooks.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/ash
+
+run_hook() {
+    export IP=172.16.42.1
+
+    mount -t proc -o nodev,noexec,nosuid proc /proc || echo "Couldn't mount /proc"
+    mount -t configfs -o nodev,noexec,nosuid configfs /sys/kernel/config || echo "Couldn't mount /sys/kernel/config"
+    mkdir -p /dev/pts
+    mount -t devpts devpts /dev/pts || echo "Couldn't mount /dev/pts"
+
+    /usr/bin/usb-tethering
+    start_udhcpd
+
+    telnetd -b "$IP:23" -l sh
+    while true; do
+        sleep 1
+    done
+}
+
+start_udhcpd() {
+    # Only run once
+    [ -e /etc/udhcpd.conf ] && return
+
+    # Skip if disabled
+    # shellcheck disable=SC2154
+    if [ "$deviceinfo_disable_dhcpd" = "true" ]; then
+        echo "NOTE: start of dhcpd is disabled (deviceinfo_disable_dhcpd)"
+        touch /etc/udhcpcd.conf
+        return
+    fi
+
+    echo "Starting udhcpd"
+    # Get usb interface
+    INTERFACE=""
+    ifconfig rndis0 "$IP" 2>/dev/null && INTERFACE=rndis0
+    if [ -z $INTERFACE ]; then
+        ifconfig usb0 "$IP" 2>/dev/null && INTERFACE=usb0
+    fi
+    if [ -z $INTERFACE ]; then
+        ifconfig eth0 "$IP" 2>/dev/null && INTERFACE=eth0
+    fi
+
+    if [ -z $INTERFACE ]; then
+        echo "  Could not find an interface to run a dhcp server on"
+        echo "  Interfaces:"
+        ip link
+        return
+    fi
+
+    echo "  Using interface $INTERFACE"
+
+    # Create /etc/udhcpd.conf
+    {
+        echo "start 172.16.42.2"
+        echo "end 172.16.42.2"
+        echo "auto_time 0"
+        echo "decline_time 0"
+        echo "conflict_time 0"
+        echo "lease_file /var/udhcpd.leases"
+        echo "interface $INTERFACE"
+        echo "option subnet 255.255.255.0"
+    } >/etc/udhcpd.conf
+
+    echo "  Start the dhcpcd daemon (forks into background)"
+    udhcpd
+}
---
+++ main/hook-debug-shell/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+build() {
+    add_runscript
+    add_binary bash
+    add_binary id
+    add_binary usb-tethering
+    add_binary mount
+    add_file /etc/kupfer/deviceinfo
+}
+
+help() {
+    cat <<HELPEOF
+This hook spins up a telnet server in the early boot phase to debug stuff
+HELPEOF
+}
