a9282d28d3393975d781572abb23ee4910041047
InsanePrawn <insane.prawny@gmail.com>
2022-02-06 19:40:20 +0100

    boot/android-bootimg-update: s/boot.img/aboot.img/g

--- boot/android-bootimg-updater/update-bootimg.sh
+++ boot/android-bootimg-updater/update-bootimg.sh
@@ -18,7 +18,7 @@ dir=$(mktemp -d)
 
 cat /boot/Image.gz "/boot/dtbs/$deviceinfo_dtb.dtb" >$dir/Image.gz-dtb
 
-echo "Generating new boot.img"
+echo "Generating new aboot.img"
 mkbootimg \
   --kernel $dir/Image.gz-dtb \
   --ramdisk /boot/initramfs-linux.img \
@@ -29,59 +29,59 @@ mkbootimg \
   --tags_offset "$deviceinfo_flash_offset_tags" \
   --pagesize "$deviceinfo_flash_pagesize" \
   --cmdline "kupfer earlymodules=${deviceinfo_modules_initfs// /,} console=tty0" \
-  -o /boot/boot.img
+  -o /boot/aboot.img
 
 qhypstub_offset="508KiB"
 qhypstub_size="4KiB"
 lk2nd_bootimg_offset="512KiB"
 
-flash_file="/boot/boot.img"
+flash_file="/boot/aboot.img"
 if [[ "$deviceinfo_lk2nd" == "true" ]]; then
-  echo "Generating new boot.bin for lk2nd device"
-  flash_file="/boot/boot.bin"
-  dd if=/dev/zero of=$flash_file bs=$lk2nd_bootimg_offset count=1
-  dd of=$flash_file bs=$qhypstub_offset count=1 conv=notrunc if=/boot/lk2nd.img
-  dd of=$flash_file bs=$qhypstub_size count=1 conv=notrunc seek=$qhypstub_offset oflag=seek_bytes if=/boot/qhypstub.bin
-  dd of=$flash_file conv=notrunc oflag=append if=/boot/boot.img
+  echo "Generating new aboot.bin for lk2nd device"
+  flash_file="/boot/aboot.bin"
+  dd if=/dev/zero of="$flash_file" bs="$lk2nd_bootimg_offset" count=1
+  dd of="$flash_file" bs="$qhypstub_offset" count=1 conv=notrunc if=/boot/lk2nd.img
+  dd of="$flash_file" bs="$qhypstub_size" count=1 conv=notrunc seek="$qhypstub_offset" oflag=seek_bytes if=/boot/qhypstub.bin
+  dd of="$flash_file" conv=notrunc oflag=append if=/boot/aboot.img
 fi
 
 AB_SLOT_SUFFIX=$(grep -o 'androidboot\.slot_suffix=..' /proc/cmdline | cut -d "=" -f2)
-BOOT_PART="/dev/disk/by-partlabel/boot${AB_SLOT_SUFFIX}"
+ABOOT_PART="/dev/disk/by-partlabel/boot${AB_SLOT_SUFFIX}"
 
-if [ -e "$BOOT_PART" ]; then
+if [ -e "$ABOOT_PART" ]; then
   echo "Running on the device"
 
-  BOOT_PART="$(readlink -f "$BOOT_PART")"
+  ABOOT_PART="$(readlink -f "$ABOOT_PART")"
 
-  dd if="$BOOT_PART" of=/tmp/boot.bin
+  dd if="$ABOOT_PART" of=/tmp/aboot.bin
   if [[ "$deviceinfo_lk2nd" == "true" ]]; then
-    dd if=/tmp/boot.bin of=/tmp/boot.img skip=$lk2nd_bootimg_offset iflag=skip_bytes
+    dd if=/tmp/aboot.bin of=/tmp/aboot.img skip="$lk2nd_bootimg_offset" iflag=skip_bytes
   else
-    cp /tmp/boot.bin /tmp/boot.img
+    cp /tmp/aboot.bin /tmp/aboot.img
   fi
 
   permanent=false
-  if [[ "$(file /tmp/boot.img)" != "/tmp/boot.img: data" ]]; then
-    cmdline=$(unpack_bootimg --boot_img /tmp/boot.img --out "$dir" | grep -E "^command line args")
-    if [[ $cmdline == *"kupfer"* ]]; then
+  if [[ "$(file /tmp/aboot.img)" != "/tmp/aboot.img: data" ]]; then
+    cmdline=$(unpack_bootimg --boot_img /tmp/aboot.img --out "$dir" | grep -E "^command line args")
+    if [[ "$cmdline" == *"kupfer"* ]]; then
       permanent=true
     fi
   fi
 
-  if [[ $permanent == "true" ]]; then
-    echo "Running with permanent boot.img"
-    echo "Flashing new boot.img"
-    dd if=$flash_file of="$BOOT_PART"
+  if [[ "$permanent" == "true" ]]; then
+    echo "Running with permanent aboot.img"
+    echo "Flashing new aboot.img"
+    dd if="$flash_file" of="$ABOOT_PART"
   elif [[ "$1" == "-f" ]]; then
-    echo "Flashing new boot.img"
-    dd if=$flash_file of="$BOOT_PART"
+    echo "Flashing new aboot.img"
+    dd if="$flash_file" of="$ABOOT_PART"
   else
-    echo "Running with temporary boot.img"
-    echo "Skip flashing new boot.img"
+    echo "Running with temporary aboot.img"
+    echo "Skip flashing new aboot.img"
   fi
 else
   echo "Not running on the device"
-  echo "Skip flashing new boot.img"
+  echo "Skip flashing new aboot.img"
 fi
 
-rm -rf "$dir" /tmp/boot.bin /tmp/boot.img
+rm -rf "$dir" /tmp/aboot.bin /tmp/aboot.img
