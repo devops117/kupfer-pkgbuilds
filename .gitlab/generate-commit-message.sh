88dab26c378f0f8797db1e50474289c194ba8bc5
InsanePrawn <insane.prawny@gmail.com>
2022-07-09 15:44:10 +0200

    .gitlab/generate-commit-message.sh: add pkgbuilds.git commit id, format multiline

--- .gitlab/generate-commit-message.sh
+++ .gitlab/generate-commit-message.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -e
-packages=()
+declare -A packages
 for file in $(git diff --cached --name-only --diff-filter=AR | grep ".pkg"); do
     repo="$(dirname "$file")"
     dir=$(mktemp -d)
     tar xf "$file" -C "$dir"
-    packages+=("$repo/$(grep pkgname "$dir"/.PKGINFO | cut -d " " -f 3)")
+    pkgname="$(grep pkgname "$dir"/.PKGINFO | cut -d " " -f 3)"
+    pkgver="$(grep pkgver "$dir"/.PKGINFO | cut -d " " -f 3)"
+    packages["$repo/$pkgname"]="$pkgver"
     rm -r "$dir"
 done
-list=${packages[*]}
-join=${list// /, }
-echo "Update $join"
+pkgbuilds_dir="$(dirname "${BASH_SOURCE[0]}")"
+commithash="$(cd "$pkgbuilds_dir" && git rev-parse HEAD)"
+committag="$(cd "$pkgbuilds_dir" && git describe --tags --abbrev=4 HEAD)"
+echo "Build for pkgbuilds.git commit $commithash, $committag:"
+for pkg in "${!packages[@]}"; do
+    echo "- update $pkg to ${packages[$pkg]}"
+done
