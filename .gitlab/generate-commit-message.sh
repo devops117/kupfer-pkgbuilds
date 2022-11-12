9431f4cb3a2b0895cd20091691e8630ff5b9a335
InsanePrawn <insane.prawny@gmail.com>
2022-11-12 15:49:19 +0100

    gitlab-ci: generate-commit-message.sh: don't fail but notify when unable to retrieve PKGBUILDs git tag

--- .gitlab/generate-commit-message.sh
+++ .gitlab/generate-commit-message.sh
@@ -12,7 +12,7 @@ for file in $(git diff --cached --name-only --diff-filter=AR | grep ".pkg"); do
 done
 pkgbuilds_dir="$(dirname "${BASH_SOURCE[0]}")"
 commithash="$(cd "$pkgbuilds_dir" && git rev-parse HEAD)"
-committag="$(cd "$pkgbuilds_dir" && git describe --tags --abbrev=4 HEAD)"
+committag="$(cd "$pkgbuilds_dir" && ( git describe --tags --abbrev=4 HEAD || (echo "(couldn't retrieve git tag)" | tee /dev/stderr) ) )"
 echo "Build for pkgbuilds.git commit $commithash, $committag:"
 for pkg in "${!packages[@]}"; do
     echo "- update $pkg to ${packages[$pkg]}"
